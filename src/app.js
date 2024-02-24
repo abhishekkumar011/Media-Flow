d39a8bc84cd953bb84600eda33c225e21fb53a8a
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-02-24 09:32:05 +0530

    feat: Add middleware for request body parsing, static files, and cookie parsing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookiesParser from "cookies-parser";
 
 const app = express();
 
@@ -9,5 +10,10 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookiesParser());
 
 export { app };
