719816b40053c26061cdedb4f7874aee2ae31e70
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-04-29 08:54:00 +0530

    feat: Implement video like toggle functionality

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,32 @@
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!videoId.trim()) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const likedAlready = await Like.findOne({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  if (likedAlready) {
+    await Like.findByIdAndDelete(likedAlready?._id);
+
+    return res.status(200).json(new ApiResponse(200, { isLiked: false }));
+  }
+
+  await Like.create({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  return res.status(200).json(new ApiResponse(200, { isLiked: true }));
+});
+
+export { toggleVideoLike };
