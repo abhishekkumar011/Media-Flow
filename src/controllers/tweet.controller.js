a2402394724636ee532b10d2e6a910dffeb3fc5f
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-03 08:39:25 +0530

    feat: Add endpoint to fetch a user's tweets with detailed information

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,7 +1,102 @@
+import mongoose from "mongoose";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Tweet } from "../models/tweet.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "userId is required");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "user not found");
+  }
+
+  const tweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likeDetails",
+        pipeline: [
+          {
+            $project: {
+              likedBy: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likeDetails",
+        },
+        ownerDetails: {
+          $first: "$ownerDetails",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likeDetails.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+
+    {
+      $project: {
+        content: 1,
+        ownerDetails: 1,
+        likesCount: 1,
+        createdAt: 1,
+        isLiked: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "tweets successfully fetched"));
+});
 
 const createTweet = asyncHandler(async (req, res) => {
   const { content } = req.body;
@@ -85,4 +180,4 @@ const deleteTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Tweet successfully deleted"));
 });
 
-export { createTweet, updateTweet, deleteTweet };
+export { getUserTweets, createTweet, updateTweet, deleteTweet };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweet,
   deleteTweet,
+  getUserTweets,
   updateTweet,
 } from "../controllers/tweet.controller.js";
 
@@ -11,5 +12,6 @@ router.use(verifyJWT);
 
 router.route("/").post(createTweet);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router.route("/user/:userId").get(getUserTweets);
 
 export default router;
