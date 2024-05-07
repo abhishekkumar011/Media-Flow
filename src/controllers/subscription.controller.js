d83d2001c99d5a4f3d1d92adf66c24a5b4c0344e
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-07 08:55:03 +0530

    feat: Implement getUserChannelSubscribers endpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -41,4 +43,86 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     );
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!channelId) {
+    throw new ApiError(400, "ChannelId is required");
+  }
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(400, "Channel not found");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscribers",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribedToSubscriber",
+            },
+          },
+
+          {
+            $addFields: {
+              subscribedToSubscriber: {
+                $cond: {
+                  if: {
+                    $in: [channelId, "$subscribedToSubscriber.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+              subscribersCount: {
+                $size: "$subscribedToSubscriber",
+              },
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $unwind: "$subscriber",
+    },
+
+    {
+      $project: {
+        _id: 0,
+        subscriber: {
+          _id: 1,
+          username: 1,
+          fullName: 1,
+          "avatar.url": 1,
+          subscribedToSubscriber: 1,
+          subscribersCount: 1,
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, "subscribers successfully fetched")
+    );
+});
+
+export { toggleSubscription, getUserChannelSubscribers };
