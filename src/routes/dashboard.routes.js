bf9950ad1018051c3d644df91e28d6099833b53b
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-10 16:43:41 +0530

    feat: Add getChannelVideos endpoint

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getChannelStats } from "../controllers/dashboard.controller.js";
+import {
+  getChannelStats,
+  getChannelVideos,
+} from "../controllers/dashboard.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
 
 export default router;
