d70ec9d328521be30296d969999178a2b876f290
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-10 11:03:29 +0530

    feat: Implement getChannelStats endpoint

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
 app.use("/api/v1/likes", likeRouter);
@@ -30,6 +31,7 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
